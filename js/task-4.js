d26438e896a2996a040ef964534c9d16a24b498a
MarinaN369 <143936204+MarinaN369@users.noreply.github.com>
2023-12-18 15:02:59 +0200

    hw-js-05

--- js/task-4.js
+++ js/task-4.js
@@ -1,6 +1,6 @@
 const getTotalBalanceByGender = (users, gender) => { return users.filter(user => user.gender === gender).reduce((total, user) => total + user.balance, 0); };
 
-const allUserse = [
+const generalUsers = [
     {
     name: "Moore Hensley",
     gender: "male",
